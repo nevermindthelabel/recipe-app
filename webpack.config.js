fd034e584b1d8c6169f6bbc94483d740297117aa
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-03 07:15:07 -0700

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const path = require('path');
 
-}
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    fileName: 'bundle.js'
+  },
+  mode: 'development'
+};
