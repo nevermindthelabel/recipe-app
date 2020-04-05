ad982ed7166382641071f5db050cfeab056a8680
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-04 23:24:34 -0700

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/js/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    fileName: 'bundle.js'
-  },
   devServer: {
-    contentBase: './dist'
-  }
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
