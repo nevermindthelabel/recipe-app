2a63ac1ab2f639554de25f293a2e8380f8c49275
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-06 21:24:21 -0700

    added error handeling

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
 async function getResults(query) {
-  const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-  console.log(res)
+  try {
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+    const recipes =  res.data.recipes
+    console.log(recipes)
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-getResults('pancake');
+getResults('ice cream');
 
