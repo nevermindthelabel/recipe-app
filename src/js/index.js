b1fd8bebcce8ebe2ff651c7cb6a32b8170fab252
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-06 21:36:46 -0700

    imported searchResults method

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,5 @@
-import axios from "axios";
+import Search from './models/Search';
 
-async function getResults(query) {
-  try {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    const recipes =  res.data.recipes
-    console.log(recipes)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-getResults('ice cream');
-
+const search = new Search('pizza');
+console.log(search);
+console.log(search.getResults());
