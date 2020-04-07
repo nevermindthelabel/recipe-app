7a54c8ed48e8b8c0f1be7d7fc191c350b64021bb
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-06 21:15:23 -0700

    updated index with mock search

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-console.log('test');
+import axios from "axios";
+
+async function getResults(query) {
+  const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
+  console.log(res)
+}
+
+getResults('pancake');
+
