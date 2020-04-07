5cbfa0122006c5730db391108f68b04df92ccf24
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-06 21:43:28 -0700

    added todos for app state

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 import Search from './models/Search';
 
+/* App Global State
+ToDo - Search Obj
+ToDo - Current recipe obj
+ToDo - Shopping list obj
+ToDo - Liked recipes
+*/
+const state = {};
+
 const search = new Search('pizza');
 console.log(search);
 console.log(search.getResults());
