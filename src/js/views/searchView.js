e2674c354d8ee5d6112c8f5bbed9ce7245c669c8
Matt Kilcup <nevermindthelabel@gmail.com>
2020-04-08 21:17:32 -0700

    added function to clear input text

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -2,6 +2,8 @@ import { elements } from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => elements.searchInput.value = '';
+
 const renderRecipe = recipe => {
   const markup = `
       <li>
